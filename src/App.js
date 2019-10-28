eb7d2560fbacea80221c9eac25f84317492ce322
jezer-oliveira <31749374+jezer-oliveira@users.noreply.github.com>
2019-10-28 20:00:36 -0300

    exemplo visto em aula

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ProdutoPai from './produto/ProdutoPai';
+
+function App() {
+  return (
+    <div>
+      <ProdutoPai />
+    </div>
+  );
+}
+
+export default App;
